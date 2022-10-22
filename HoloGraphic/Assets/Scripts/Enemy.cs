84cf8b4061151816bd6ec233067549a548df2f6c
Ricky102 <48187498+Ricky102@users.noreply.github.com>
2022-10-22 18:05:22 -0400

    General changes and Adding Enemy, Player
    Changed Dos to DDos. Turned off skybox. Added Enemy and Player as images. Added Enemy Script with Spider, which inherits Enemy.

--- HoloGraphic/Assets/Scripts/Enemy.cs
+++ HoloGraphic/Assets/Scripts/Enemy.cs
@@ -2,29 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
-public class Card : ScriptableObject
+public class Enemy : MonoBehaviour
 {
+
     public new string name;
     public string description;
 
     public Sprite artwork;
 
-    public int memoryCost;
     public int attack;
     public int defense;
     public int health;
-
-    public Enemy enemy;
-
-    public void Print()
-    {
-        Debug.Log(name + ": " + description);
-    }
-
+    public string specialAttack;
     public void Attack()
     {
-        enemy.health -= attack;
+        Debug.Log("Attack!");
     }
-    
+
+    //public void specialAttack()
+    //{
+    //    Debug.Log("Launching Special Attack!");
+    //}
 }
---
+++ HoloGraphic/Assets/Scripts/Spider.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spider : Enemy
+{
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Implement turn based system
+        Attack();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
