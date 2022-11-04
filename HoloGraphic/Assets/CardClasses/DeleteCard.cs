1b4a9e63691be7878e33150b6d1f30e7337d6c61
Novanduh <sampratisinha@gmail.com>
2022-11-04 13:25:54 -0400

    added NueDeck

--- HoloGraphic/Assets/CardClasses/DeleteCard.cs
+++
@@ -1,8 +0,0 @@
-public class DeleteCard : BaseCard
-{
-    public void createDeleteCard(string name, string description)
-    {
-        setCardName(name);
-        setBackendDescription(description);
-    }
-}
